f4b09467931fc49ad47c67b35e400a3810a3a8ca
Ella Tanttu <ellajtanttu@gmail.com>
2022-03-06 14:25:03 -0800

    remove form 2 from application, opting for x's in main list

--- fitFixApp/components/Form2.js
+++ fitFixApp/components/Form2.js
@@ -1,29 +1,27 @@
-import * as React from 'react';
-import { StyleSheet, Button, View, Text } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import ListItem from './ListItem';
+// import * as React from 'react';
+// import { StyleSheet, Button, View, Text } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import ListItem from './ListItem';
 
+// export default function Form2({navigation}) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>THIS IS THE FORM2 COMPONENT</Text>
+//       <StatusBar style="auto" />
+//       <Button
+//         title="Go to List"
+//         onPress={() => navigation.navigate('List')}
+//       />
+//       <ListItem />
+//     </View>
+//   );
+// }
 
-
-export default function Form2({navigation}) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>THIS IS THE FORM2 COMPONENT</Text>
-      <StatusBar style="auto" />
-      <Button
-        title="Go to List"
-        onPress={() => navigation.navigate('List')}
-      />
-      <ListItem />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
