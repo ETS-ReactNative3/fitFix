1cfc553bb47ad6453a7a27a2b59e0ca429a7512a
Ella Tanttu <ellajtanttu@gmail.com>
2022-03-06 15:25:23 -0800

    list items successfully appear on list, from props

--- fitFixApp/components/ListItem.js
+++ fitFixApp/components/ListItem.js
@@ -1,26 +1,27 @@
-import * as React from 'react';
-import { StyleSheet, Button, View, Text } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import * as React from "react";
+import { StyleSheet, Button, View, Text, TouchableOpacity } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-
-export default function ListItem({navigation}) {
+export default function ListItem({ navigation, item }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>LIST ITEM</Text>
-      <Text>Exercise Name</Text>
-      <Text>Exercise Description: fjas;ldjfls dslkfjdjk dsjfdksjf fds sdj lkjkljsdf jkljfs.</Text>
-      <Text>Equipment</Text>
-      <Text>Muscle</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableOpacity style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View>
+        <Text>{item.name}</Text>
+        {/* <Text>Exercise Name</Text>
+        <Text>Exercise Description: fjas;ldjfls dslkfjdjk dsjfdksjf fds sdj lkjkljsdf jkljfs.</Text>
+        <Text>Equipment</Text>
+        <Text>Muscle</Text> */}
+        <StatusBar style="auto" />
+      </View>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
