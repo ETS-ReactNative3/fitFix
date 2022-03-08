d95442433f69f50f0d9515f69ecdf6dd6527d1d8
Ella Tanttu <ellajtanttu@gmail.com>
2022-03-07 17:53:39 -0800

    back to original

--- fitFixApp/components/List.js
+++ fitFixApp/components/List.js
@@ -1,47 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Button, View, Text, FlatList, Modal, TouchableOpacity, ActivityIndicator } from "react-native";
-// import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Button, View, Text, FlatList, Modal, TouchableOpacity } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import ListItem from "./ListItem";
 import { uuid } from "uuidv4";
 
 export default function List({ navigation }) {
-  const [isLoading, setLoading] = useState(true);
-  const [items, setItems] = useState([]);
-
-  const getExercises = async () => {
-    try {
-      const response = await fetch("https://reactnative.dev/movies.json");
-      // const response = await fetch("https://wger.de/api/v2/exerciseinfo/");
-      const json = await response.json();
-      SVGMetadataElement(json.exercises);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
+  const [items, setItems] = useState([
+    {
+      id: 345,
+      uuid: "c788d643-150a-4ac7-97ef-84643c6419bf",
+      name: "2 Handed Kettlebell Swing"
+    },
+    {
+      id: 174,
+      uuid: "99881bdd-43d7-4c3b-82ed-9c187d0455b7",
+      name: "Abduktoren-Maschine"
+    },
+    {
+      id: 223,
+      uuid: "e56d0a17-b1b8-427d-991e-97c530c7db6a",
+      name: "Adduktoren Maschine"
     }
-  };
-
-  useEffect(() => {
-    getExercises();
-  }, []);
-
-  // const [items, setItems] = useState([
-  //   {
-  //     id: 345,
-  //     uuid: "c788d643-150a-4ac7-97ef-84643c6419bf",
-  //     name: "2 Handed Kettlebell Swing"
-  //   },
-  //   {
-  //     id: 174,
-  //     uuid: "99881bdd-43d7-4c3b-82ed-9c187d0455b7",
-  //     name: "Abduktoren-Maschine"
-  //   },
-  //   {
-  //     id: 223,
-  //     uuid: "e56d0a17-b1b8-427d-991e-97c530c7db6a",
-  //     name: "Adduktoren Maschine"
-  //   }
-  // ]);
+  ]);
 
   const deleteItem = id => {
     setItems(prevItems => {
@@ -52,21 +32,17 @@ export default function List({ navigation }) {
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <TouchableOpacity>
-              <ListItem item={item} deleteItem={deleteItem}>
-                {item.title}
-              </ListItem>
-            </TouchableOpacity>
-          )}
-        />
-      )}
+      <Text>THIS IS THE List COMPONENT</Text>
+      <StatusBar style="auto" />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <ListItem item={item} deleteItem={deleteItem} />
+            {/* <Button title="See Exercise Details" onPress={() => navigation.navigate("Details")} /> */}
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
