1e7bbd376bbb695ed98598dfdc4a3d601c61ca69
Ella Tanttu <ellajtanttu@gmail.com>
2022-03-08 12:44:50 -0800

    got flatlist to work with data variable

--- fitFixApp/components/Form.js
+++ fitFixApp/components/Form.js
@@ -12,7 +12,7 @@ const makeMuscleRequest = async () => {
     if (response.status === 200) {
       // response - object, eg { status: 200, message: 'OK' }
       console.log("muscle success stuff");
-      console.log(response.data.results[1].name);
+      console.log(response.data.results);
       return response.data.results;
     }
     return false;
@@ -22,11 +22,31 @@ const makeMuscleRequest = async () => {
   }
 };
 
-const Item = ({ name }) => (
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item"
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item"
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item"
+  }
+];
+
+const Item = ({ title }) => (
   <View>
-    <Text>{name}</Text>
+    <Text>{title}</Text>
   </View>
 );
+// const Item = ({ name }) => (
+//   <View>
+//     <Text>{name}</Text>
+//   </View>
+// );
 
 export default function Form({ navigation /* muscles equipment*/ }) {
   // for the form and selection boxes
@@ -40,27 +60,33 @@ export default function Form({ navigation /* muscles equipment*/ }) {
   const onSubmit = data => console.log(data);
 
   // render item used in the flatlist
-  const renderItem = ({ item }) => {
-    console.log("renderItem function");
+  const renderItem = ({ item }) => (
     // <CheckBox value={isSelected} onValueChange={setSelection} style={styles.checkbox} />;
-    return <Item name={item.name} />;
-  };
+    <Item title={item.title} />
+  );
+  // const renderItem = ({ item }) => {
+  //   console.log("renderItem function");
+  //   // <CheckBox value={isSelected} onValueChange={setSelection} style={styles.checkbox} />;
+  //   <Item name={item.name} />;
+  // };
 
   return (
     <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>THIS IS THE FORM1 COMPONENT</Text>
       <StatusBar style="auto" />
       <Button title="Go to List" onPress={() => navigation.navigate("List")} />
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <View /*onSubmit={handleSubmit(onSubmit)}*/>
         <Text>THIS IS inside the form tags</Text>
         {console.log("This is the in form request:")}
-        <FlatList data={makeMuscleRequest()} keyExtractor={item => item.id} renderItem={renderItem} />
+        <FlatList data={DATA} keyExtractor={item => item.id} renderItem={renderItem} />
+        {/* <FlatList data={makeMuscleRequest()} keyExtractor={item => item.id} renderItem={renderItem} /> */}
         {/* <input defaultValue="test" {...register("example")} />
         <input {...register("exampleRequired", { required: true })} />
         {errors.exampleRequired && <span>This field is required</span>} */}
 
-        <input type="submit" />
-      </form>
+        {/* <input type="submit" /> */}
+        <FlatList data={DATA} renderItem={renderItem} keyExtractor={item => item.id} />
+      </View>
     </SafeAreaView>
   );
 }
